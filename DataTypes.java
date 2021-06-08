55e5302336c17cbe5782ebb2fb50a23ae74e4dd2
syahriramadhann <ramadhansyahri0912@gmail.com>
2021-06-08 19:48:30 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,11 @@
+public class DataTypes{
+   public static void main(String args[]) {
+	String namaDepan = "Syahri";
+    String namaBelakang = "Ramadhan";
+    int usia =20;
+    int targetTahunKuliah = 4;
+    double ipk = 3.89764512;
+    char nilaiAbjad = 'A';
+    boolean tampan = true;
+   }
+}
